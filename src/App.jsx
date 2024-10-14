9bdb2535c5dfa63d30d2ffc305dd6f8883612cc1
YoussefShaban20210472 <102224925+YoussefShaban20210472@users.noreply.github.com>
2024-10-15 01:07:09 +0300

    Final result page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
-// import ResultPage from "./components/ui/ResultPage/ResultPage";
-import ResultPage from "./components/ui/ResultPage/ResultPage";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout/Layout";
+import Login from "./pages/Login/Login";
+import Home from "./pages/Home/Home";
+import ResultPage from "./pages/ResultPage/ResultPage";
+
+const routes = createBrowserRouter([
+  { path: "", element: <Login /> },
+  { path: "login", element: <Login /> },
+  {
+    path: "",
+    element: <Layout />,
+    children: [
+      { path: "home", element: <Home /> },
+      { path: "result", element: <ResultPage /> },
+      { path: "result/:type", element: <ResultPage /> },
+
+      // { path: '*', element: <NotFound /> },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<ResultPage />} />
-        <Route path="/:type" element={<ResultPage />} />
-      </Routes>
-    </Router>
-  );
+  return <RouterProvider router={routes}></RouterProvider>;
 }
 
 export default App;
